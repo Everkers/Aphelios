4f3bad114c3342ec3f23177fc6ebc96e59a0bcb0
Ziad Abouelfarah <zain.fake02@gmail.com>
2020-08-06 19:48:46 +0100

    config the fking eslint

--- app/server.js
+++ app/server.js
@@ -1,14 +1,14 @@
-const http = require('http').createServer();
-const io = require('socket.io')(http);
+const http = require("http").createServer()
+const io = require("socket.io")(http)
 
-const port = 3000;
-io.on('connection', (socket) => {
-  console.log('connected');
-  socket.on('message', (evt) => {
-    socket.broadcast.emit('message', evt);
-  });
-});
-io.on('disconnect', (evt) => {
-  console.log('disconnected');
-});
-http.listen(port, () => console.log(`server listening on port: ${port}`));
+const port = 3000
+io.on("connection", (socket) => {
+  console.log("connected")
+  socket.on("message", (evt) => {
+    socket.broadcast.emit("message", evt)
+  })
+})
+io.on("disconnect", (evt) => {
+  console.log("disconnected")
+})
+http.listen(port, () => console.log(`server listening on port: ${port}`))
